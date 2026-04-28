604ea252a08c35756a31f78cd297a02f5fe768e0
James <jamie.jj.jd@gmail.com>
2026-04-28 21:17:54 +0100

    Update Room.cs

--- Assets/Scripts/RoomScripts/Room.cs
+++ Assets/Scripts/RoomScripts/Room.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public abstract class Room : MonoBehaviour
 {
     public abstract string myName { get; }
+    //added for use within movement.cs
     public Where roomIdentity;
 
 }
